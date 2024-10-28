127514403219508d171d8442ac3fbb2e39ccce85
Ricardo Cerna <ricardo.cerna2001@gmail.com>
2024-10-27 21:12:17 -0600

    FIRST COMMIT

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DAMA_VIDEOAPP_CB100420"
+include(":app")
+ 
